fe1ef78d33ccdd7ea5fd63c225f263c0e84e84ad
bobo <187936623+bobo334@users.noreply.github.com>
2026-02-11 00:42:09 +0800

    Bump version to 1.2.3

--- gravatar-proxy.php
+++ gravatar-proxy.php
@@ -3,7 +3,7 @@
 Plugin Name: Gravatar Proxy
 Plugin URI: https://github.com/bobo334/gravatar-proxy
 Description: 本地代理 Gravatar 头像，提升加载速度
-Version: 1.2.2
+Version: 1.2.3
 Author: bobo
 License: GPL v2 or later
 */
@@ -13,7 +13,7 @@ if (!defined('ABSPATH')) {
 }
 
 // 常量定义
-define('GRAVATAR_PROXY_VERSION', '1.2.2');
+define('GRAVATAR_PROXY_VERSION', '1.2.3');
 define('GRAVATAR_PROXY_PATH', plugin_dir_path(__FILE__));
 define('GRAVATAR_PROXY_URL', plugin_dir_url(__FILE__));
 if (!defined('GRAVATAR_CACHE_DIR')) {
